4a8b3edae37aec9f71d020ceb32cf95826866cfe
codecraft26 <codescraft26@gmail.com>
2022-04-15 23:20:40 +0530

    call adapter in main activtiy

--- app/src/main/java/dev/codecraft/multipleviewholder/MainActivity.kt
+++ app/src/main/java/dev/codecraft/multipleviewholder/MainActivity.kt
@@ -2,10 +2,19 @@ package dev.codecraft.multipleviewholder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.codecraft.multipleviewholder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+        val landmarks = LandmarkModel.getLandmarks()
+        mainBinding.landmarkRecyclerview.apply {
+            adapter = LandmarkAdapter(landmarks)
+        }
+
     }
 }
